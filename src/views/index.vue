a7dd7e131409f485a9135cfdfda1ec28beef373c
kzycn <kzycn@qq.com>
2021-02-28 18:22:35 +0800

    增加开发提示

--- src/views/index.vue
+++ src/views/index.vue
@@ -19,7 +19,12 @@ import Carousel from '../components/indexComponents/Carousel'
 
 export default {
   name: 'index',
-  components: { Carousel, footermain }
+  components: { Carousel, footermain },
+  beforeCreate () {
+    this.$alert('正在开发的页面，登录功能需要请求后台，本站点后台正在开发，码云搜索：float，作者：浮生Husei，即可下载到配套源码！', '警告！！！', {
+      confirmButtonText: '确定'
+    })
+  }
 }
 </script>
 
--- src/views/register.vue
+++ src/views/register.vue
@@ -4,7 +4,12 @@
 
 <script>
 export default {
-  name: 'register'
+  name: 'register',
+  beforeCreate () {
+    this.$alert('正在开发的页面，登录功能需要请求后台，本站点后台正在开发，码云搜索：float，作者：浮生Husei，即可下载到配套源码！', '警告！！！', {
+      confirmButtonText: '确定'
+    })
+  }
 }
 </script>
 
